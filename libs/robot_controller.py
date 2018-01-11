238f5f64fb9170e7b9bbe3d2149bee8bf84ce31a
venemap <venemap@rose-hulman.edu>
2018-01-11 09:10:55 -0500

    fixed a typo

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -19,5 +19,5 @@ import time
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
     
-    # TODO: Implement the Snatch3r class as needed when working the sandox exercises
+    # TODO: Implement the Snatch3r class as needed when working the sandbox exercises
     # (and delete these comments)
