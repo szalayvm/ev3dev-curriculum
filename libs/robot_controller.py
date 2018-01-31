c34ba081724db280f0952267396844ca020f9035
szalayvm <szalayvm>
2018-01-31 09:29:30 -0500

    I changed my name in the read-me file.

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -36,7 +36,7 @@ class Snatch3r(object):
         self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
         self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
 
-    def turn_degrees(self,degrees_to_turn,turn_speed_up):
+    def turn_degrees(self,degrees_to_turn,turn_speed_sp):
 
-        self.left_motor.run_to_rel_pos(speed_sp=turn_speed_up,position_sp=-1*degrees_to_turn,stop_action='brake')
-        self.right_motor.run_to_rel_pos(speed_sp=turn_speed_up,position_sp=degrees_to_turn,stop_action='brake')
+        self.left_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=-1*degrees_to_turn,stop_action='brake')
+        self.right_motor.run_to_rel_pos(speed_sp=turn_speed_sp,position_sp=degrees_to_turn,stop_action='brake')
