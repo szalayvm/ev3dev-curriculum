de49a83dd4dc4053f0ca04c43515eb726e6328de
venemap <venemap@rose-hulman.edu>
2018-01-31 09:09:58 -0500

    update motorcontroller for m3 - updated

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -12,23 +12,26 @@
 """
 
 import ev3dev.ev3 as ev3
-import math
-import time
 
 
 class Snatch3r(object):
     """Commands for the Snatch3r robot that might be useful in many different programs."""
-    
-    # TODO: Implement the Snatch3r class as needed when working the sandbox exercises
-    # (and delete these comments)
+    def __init__(self):
+        """Constructs all necessary initial instance variables for the snatch3r class."""
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
 
-    def drive_inches(self, inches_to_target, speedDegrees):
-        left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
-        right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
-        degreeToInch = 90
-        deg = inches_to_target * degreeToInch
+        assert self.left_motor
+        assert self.right_motor
 
-        left_motor.run_to_rel_pos(speed_sp=speedDegrees, position_sp=deg, stop_action='brake')
-        right_motor.run_to_rel_pos(speed_sp=speedDegrees, position_sp=deg, stop_action='brake')
-        left_motor.wait_while(ev3.Motor.STATE_RUNNING)
-        right_motor.wait_while(ev3.Motor.STATE_RUNNING)
+    def drive_inches(self, inches_to_target, speed_degrees):
+        """ Takes in inches needed for travel and speed at which to travel and makes robot move that distance at that speed.
+        Input: inches_to_target (int), speedDegrees(int)
+        Output: None"""
+        degree_to_inch = 90
+        deg = inches_to_target * degree_to_inch
+
+        self.left_motor.run_to_rel_pos(speed_sp=speed_degrees, position_sp=deg, stop_action='brake')
+        self.right_motor.run_to_rel_pos(speed_sp=speed_degrees, position_sp=deg, stop_action='brake')
+        self.left_motor.wait_while(ev3.Motor.STATE_RUNNING)
+        self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
