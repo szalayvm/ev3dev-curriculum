b9c1c63cf0325616400adc6a99090105dab9f380
szalayvm <szalayvm>
2018-02-06 20:47:45 -0500

    Finished 4.

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -95,6 +95,15 @@ class Snatch3r(object):
         self.arm_motor.wait_while(ev3.Motor.STATE_RUNNING)
         ev3.Sound.beep().wait()
 
+    def loop_forever(self):
+        # This is a convenience method that I don't really recommend for most programs other than m5.
+        #   This method is only useful if the only input to the robot is coming via mqtt.
+        #   MQTT messages will still call methods, but no other input or output happens.
+        # This method is given here since the concept might be confusing.
+        self.running = True
+        while self.running:
+            time.sleep(0.1)  # Do nothing (except receive MQTT messages) until an MQTT message calls shutdown.
+
     def shutdown(self):
         """ Shuts the robot down
          Input: None
