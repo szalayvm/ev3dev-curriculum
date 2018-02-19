1a0fba4f5e0384eb31ec11f58157d1077051b1c3
setinajb <setinajb@rose-hulman.edu>
2018-02-19 13:10:29 -0500

    can drive with buttons, colors not working properly

--- projects/setinajb/ev3_project/ev3_remote_drive.py
+++ projects/setinajb/ev3_project/ev3_remote_drive.py
@@ -3,19 +3,30 @@ import robot_controller as robo
 import ev3dev.ev3 as ev3
 import time
 
+#
+
 
 class MyDelegate(object):
-    def __init__(self):
-        pass
+    def __init__(self, robot):
+        self.robot = robot
+
+    def drive_forever(self, left_speed, right_speed):
+        print('told to drive at', left_speed, right_speed)
+        self.robot.drive_forever(left_speed, right_speed)
+
+    def stop_motors(self):
+        print('told to stop')
+        self.robot.drive_forever(0, 0)
 
 
 def main():
+    print("Running")
     robot = robo.Snatch3r()
-    my_delegate = MyDelegate()
+    my_delegate = MyDelegate(robot)
     mqtt_client = com.MqttClient(my_delegate)
     mqtt_client.connect_to_pc()
-    while True:
 
+    while True:
         dp = 0
         if robot.color_sensor.color == ev3.ColorSensor.COLOR_YELLOW:
             dp = -10
@@ -25,6 +36,7 @@ def main():
             dp = -5
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
             ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+            robot.turn_degrees(360, 900)
         if robot.color_sensor.color == ev3.ColorSensor.COLOR_RED:
             dp = 15
             ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
@@ -33,6 +45,7 @@ def main():
             mqtt_client.send_message("change_points", [dp])
             time.sleep(4)
             ev3.Leds.all_off()
+        time.sleep(0.1)
 
 
 # ----------------------------------------------------------------------
